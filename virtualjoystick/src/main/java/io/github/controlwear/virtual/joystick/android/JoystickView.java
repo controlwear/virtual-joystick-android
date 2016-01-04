fc0efa45e06c7e52465a2eefb5a2b9404427f1da
Damien Brun <makowildcat@gmail.com>
2016-01-04 14:21:48 -0500

    Create Constructors

--- virtualjoystick/src/main/java/io/github/controlwear/virtual/joystick/android/JoystickView.java
+++ virtualjoystick/src/main/java/io/github/controlwear/virtual/joystick/android/JoystickView.java
@@ -1,5 +1,29 @@
 package io.github.controlwear.virtual.joystick.android;
 
 
-public class JoystickView {
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class JoystickView extends View {
+
+
+    /*
+    CONSTRUCTORS
+     */
+
+
+    public JoystickView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+    public JoystickView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+    public JoystickView(Context context) {
+        super(context);
+    }
 }
