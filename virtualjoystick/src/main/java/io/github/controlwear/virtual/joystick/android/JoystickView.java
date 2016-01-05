209acdaf7c51caf540ec89a084781f36a44fdac8
Damien Brun <makowildcat@gmail.com>
2016-01-05 11:11:50 -0500

    Add attributes in order to customize button and border color

--- virtualjoystick/src/main/java/io/github/controlwear/virtual/joystick/android/JoystickView.java
+++ virtualjoystick/src/main/java/io/github/controlwear/virtual/joystick/android/JoystickView.java
@@ -2,10 +2,16 @@ package io.github.controlwear.virtual.joystick.android;
 
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class JoystickView extends View {
+
+
+    private int mButtonColor;
+    private int mBorderColor;
 
 
     /*
@@ -20,10 +26,38 @@ public class JoystickView extends View {
 
     public JoystickView(Context context, AttributeSet attrs) {
         super(context, attrs);
+
+        TypedArray styledAttributes = context.getTheme().obtainStyledAttributes(
+                attrs,
+                R.styleable.JoystickView,
+                0, 0
+        );
+
+        try {
+            mButtonColor = styledAttributes.getColor(R.styleable.JoystickView_buttonColor, Color.BLACK);
+            mBorderColor = styledAttributes.getColor(R.styleable.JoystickView_borderColor, Color.BLACK);
+        } finally {
+            styledAttributes.recycle();
+        }
     }
 
 
     public JoystickView(Context context) {
         super(context);
+    }
+
+
+    /*
+    SETTERS
+     */
+
+
+    public void setButtonColor(int newColor){
+        mButtonColor=newColor;
+    }
+
+
+    public void setBorderColor(int newColor){
+        mBorderColor=newColor;
     }
 }
